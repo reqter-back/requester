496bb3986f96a5f7bdf03c8d20a54d4f10280ece
fakhrad <fakhrad@openratio.com>
2019-08-04 16:44:25 +0430

    access_token api for requester app updated

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,6 @@
 var jwt = require('jsonwebtoken');
 const config = require('../config');
-
+const axios  = require('axios');
 function loadHeaders(req, res, next) {
   if (req.headers.clientid)
     req.clientId = req.headers.clientid;
@@ -37,4 +37,46 @@ function getAppToken(req, res, next) {
 }
  exports.loadHeaders = loadHeaders;
  exports.verifyToken = verifyToken;
- exports.gettoken = getAppToken;
+ exports.gettoken = [
+  (req, res, next)=>{
+      var apiRoot = process.env.APPS_ACCESSTOKEN_API || "https://app-ipanel.herokuapp.com";
+      var config = {
+        url : "/auth/token",
+        baseURL : apiRoot,
+        method : "post",
+        data : {
+          "username" : "loan@reqter.com",
+          "password" : "logrezaee24359"
+        },
+        headers : {
+            'clientid' : req.headers.clientid
+        }
+      };
+      console.log(config);
+      axios(config).then(function (response) {
+        res.send({"access_token" : response.data.access_token});
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            res.status(error.response.status).send(error.response.data);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+            res.status(204).send("No response from server");
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+            res.status(500).send(error.message);
+          }
+          console.log(error.config);
+          res.status(400).send(error.config);
+        });
+  }
+];
