236c2bba060bf40d869b63191821199358e7f3cb
fakhrad <fakhrad@openratio.com>
2019-08-08 12:40:55 +0430

    myrequest api added

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var controller = require('../controllers/requestController');
-var auth = require('../controllers/auth');
+var controller = require("../controllers/requestController");
+var auth = require("../controllers/auth");
 
 router.get("/allnew", auth.verifyToken, controller.getNewapplications);
 router.get("/opened", auth.verifyToken, controller.getOpenedApplications);
 router.put("/open/:id", auth.verifyToken, controller.openApplication);
 router.put("/reject/:id", auth.verifyToken, controller.rejectApplication);
 router.post("/submit", auth.verifyToken, controller.submit);
-router.get("/", auth.verifyToken, controller.myRequests);
+router.get("/:contentType", auth.verifyToken, controller.myRequests);
 router.get("/:id/offers", auth.verifyToken, controller.getRequestsOffers);
 module.exports = router;
