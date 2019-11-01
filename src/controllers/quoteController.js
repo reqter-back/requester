29cc58fceddc75b9f9223327ae5db2128ddbb39d
fakhrad <fakhrad@openratio.com>
2019-11-01 18:26:42 +0330

    won and lost offers added to quotes

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -201,18 +201,18 @@ exports.myoffers = [
 
 exports.lostoffers = [
   (req, res, next) => {
+    var q = req.query || {};
+    if (q) {
+      q["sys.issuer"] = req.userId;
+    }
+    console.log(q);
     var apiRoot =
       process.env.CONTENT_DELIVERY_API || "https://app-dpanel.herokuapp.com";
     var config = {
-      url: "/graphql",
+      url: "/contents/search",
       baseURL: apiRoot,
       method: "get",
-      params: {
-        query:
-          '{contents(contentType : "' +
-          req.params.contentType +
-          '"){ fields, _id, sys{issuer issueDate} }  }'
-      },
+      params: req.query,
       headers: {
         authorization: req.headers.authorization,
         clientid: req.spaceId.toString()
@@ -221,9 +221,7 @@ exports.lostoffers = [
     console.log(config);
     axios(config)
       .then(function(response) {
-        if (response.data && response.data.data && response.data.data.contents)
-          res.send(response.data.data.contents);
-        else res.send(response.data);
+        res.send(response.data);
       })
       .catch(function(error) {
         if (error.response) {
@@ -249,23 +247,51 @@ exports.lostoffers = [
       });
   }
 ];
-
 exports.issueOffer = [
   (req, res, next) => {
-    broker
-      .sendRPCMessage(
-        { body: req.body, userId: req.userId, spaceId: req.spaceId },
-        "submitcontent"
-      )
-      .then(result => {
-        var obj = JSON.parse(result.toString("utf8"));
-        if (!obj.success) {
-          if (obj.error) {
-            return res.status(500).json(obj);
-          }
+    var q = req.query || {};
+    if (q) {
+      q["sys.issuer"] = req.userId;
+    }
+    console.log(q);
+    var apiRoot =
+      process.env.CONTENT_DELIVERY_API || "https://app-dpanel.herokuapp.com";
+    var config = {
+      url: "/contents/search",
+      baseURL: apiRoot,
+      method: "get",
+      params: req.query,
+      headers: {
+        authorization: req.headers.authorization,
+        clientid: req.spaceId.toString()
+      }
+    };
+    console.log(config);
+    axios(config)
+      .then(function(response) {
+        res.send(response.data);
+      })
+      .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          res.status(error.response.status).send(error.response.data);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+          res.status(204).send("No response from server");
         } else {
-          res.status(201).json(obj.data);
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          res.status(500).send(error.message);
         }
+        console.log(error.config);
+        res.status(400).send(error.config);
       });
   }
 ];
