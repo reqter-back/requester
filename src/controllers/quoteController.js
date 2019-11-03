1e06df7e8030b458d82fbb5444bf00163a96181a
fakhrad <fakhrad@openratio.com>
2019-11-03 17:41:38 +0330

    issue offer code reverted to the previous commit

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -247,49 +247,20 @@ exports.lostoffers = [
 ];
 exports.issueOffer = [
   (req, res, next) => {
-    var q = req.query || {};
-    if (q) {
-      q["sys.issuer"] = req.userId;
-    }
-    console.log(q);
-    var apiRoot =
-      process.env.CONTENT_DELIVERY_API || "https://app-dpanel.herokuapp.com";
-    var config = {
-      url: "/contents/search",
-      baseURL: apiRoot,
-      method: "get",
-      params: req.query,
-      headers: {
-        authorization: req.headers.authorization,
-        clientid: req.spaceId.toString()
-      }
-    };
-    console.log(config);
-    axios(config)
-      .then(function(response) {
-        res.send(response.data);
-      })
-      .catch(function(error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-          res.status(error.response.status).send(error.response.data);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-          res.status(204).send("No response from server");
+    broker
+      .sendRPCMessage(
+        { body: req.body, userId: req.userId, spaceId: req.spaceId },
+        "submitcontent"
+      )
+      .then(result => {
+        var obj = JSON.parse(result.toString("utf8"));
+        if (!obj.success) {
+          if (obj.error) {
+            return res.status(500).json(obj);
+          }
         } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-          res.status(500).send(error.message);
+          res.status(201).json(obj.data);
         }
-        console.log(error.config);
-        res.status(400).send(error.config);
       });
   }
 ];
