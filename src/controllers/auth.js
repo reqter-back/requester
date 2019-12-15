002ad0d43fd132140b59c88539a18c0388962111
fakhrad <fakhrad@openratio.com>
2019-12-15 12:39:24 +0330

    check server api added

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -148,4 +148,44 @@ exports.gettoken = [
   }
 ];
 
+exports.checkServer = [
+  (req, res, next) => {
+    var apiRoot = "http://cda-service:8053";
+    var config = {
+      url: "/contents/test",
+      baseURL: apiRoot,
+      method: "get",
+      params: req.query
+    };
+    console.log(config);
+    axios(config)
+      .then(function(response) {
+        var output = [];
+        console.log(response.data);
+        res.send(response.data);
+      })
+      .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          res.status(error.response.status).send(error.response.data);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+          res.status(204).send("No response from server");
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          res.status(500).send(error.message);
+        }
+        console.log(error.config);
+        res.status(400).send(error.config);
+      });
+  }
+];
 exports.logout = logout;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,4 +3,5 @@ var router = express.Router();
 var controller = require("../controllers/auth");
 router.delete("/logout", controller.loadHeaders, controller.logout);
 router.post("/token", controller.loadHeaders, controller.gettoken);
+router.post("/checkserver", controller.loadHeaders, controller.checkServer);
 module.exports = router;
